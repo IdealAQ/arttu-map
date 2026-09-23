039fe8f89f7ff149213b2bbf64cd6cbc245baa36
Arttu Joona Juhani Marsela <arttu.marsela@gmail.com>
2026-09-23 20:22:44 +0300

    created main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from data_handler import DataHandler
+
+
+
+# Define the time interval from which to extract the data
+time_interval = ("2026-06-30 00:00:00", "2026-07-03 23:59:59")
+
+
+# Initialize the DataHandler with the path to the testing data directory
+data_handler = DataHandler(data_directory="testing_data")
+
